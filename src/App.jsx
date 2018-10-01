967cceb6f6f154cc80fcad67332e3efd04376846
anthonySchafrik <anthonySchafrik@gmail.com>
2018-10-01 03:01:22 -0400

    add basic mock set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Container from "./components/Container";
 
-class App extends React.Component {}
+class App extends React.Component {
+  render() {
+    return (
+      <div id="main">
+        <Container />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
