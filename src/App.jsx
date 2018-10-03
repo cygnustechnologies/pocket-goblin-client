7b5ce04f6ae30a17244597ab528c96c6c716e61e
anthonySchafrik <anthonySchafrik@gmail.com>
2018-10-02 22:20:16 -0400

    add debt components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Container from "./components/Container";
+import FinancialSnapShot from "./components/FinancialSnapShot";
 
 class App extends React.Component {
   render() {
     return (
       <div id="main">
-        <Container />
+        <FinancialSnapShot />
       </div>
     );
   }
